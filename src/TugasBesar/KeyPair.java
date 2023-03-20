c60ca0d9810c748f0688f7d22e2b0e5d767f26cb
zaza <sitiazzaleaannisa@gmail.com>
2023-03-20 19:54:30 +0800

    penambahan random picker untuk mencari p dan q, pembuatan phi(n) oleh zaza

--- src/TugasBesar/KeyPair.java
+++ src/TugasBesar/KeyPair.java
@@ -5,6 +5,7 @@
 package TugasBesar;
 
 import java.math.*;
+import java.security.SecureRandom;
 import java.util.*;
 
 /**
@@ -34,8 +35,9 @@ public class KeyPair {
         }
         return result;
     } 
-    
-    public static List<Integer> primeList(int lower, int upper, List<Integer> list){
+
+    private List<Integer> primeList = new ArrayList<>();    
+    public static List<Integer> getPrimes(int lower, int upper, List<Integer> list){
         for (int i = lower; i <= upper; i++){
             if (isPrime (i)) {
                 list.add(i);
@@ -44,13 +46,23 @@ public class KeyPair {
         return list;
     }
 
+    public int randomPicker(int bound){
+        SecureRandom rand = new SecureRandom();
+        return rand.nextInt(bound);
+    }
+    
 
-//    public KeyPair(List primeList) {
+//    public KeyPair(List getPrimes) {
     public KeyPair() {
-//    buat prime generator antara 1000 s/d 9999, masukkan di ArrayList, aagr bisa langsung memilih secara acak 
+//    buat prime generator antara 1000 s/d 9999, masukkan di ArrayList, agar bisa langsung memilih secara acak 
+        getPrimes(1000, 9999, primeList);
 //    generate random antara 0 s/d lst.size-1 untuk mendapatkan index p dan q
+        this.p = primeList.get(randomPicker(primeList.size()));
+        this.q = primeList.get(randomPicker(primeList.size()));
 //    hitung n = p * q
+        int n = p * q;
 //    hitung phi(n) = (p-1)(q-1)
+        int phi_n = (p-1)*(q-1);
 //    pilih kunci publik e yang relatif prima terhadap phi(n). (Kudu bikin algo generator coprime)
 //    bangkitkan kunci privat dengan persamaan e*d = 1 mod(phi(n)), di mana d = (1 + k(phi(n))) / n adalah integer
     }
